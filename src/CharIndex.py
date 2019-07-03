0a9494eda70b127ecfd0537c6f9b5b8c27fc6b65
mmsamiei <mohmahsamiei@gmail.com>
2019-07-03 21:31:13 +0430

    add Dataset transformers and CharIndex useful class :) God is Greater!

---
+++ src/CharIndex.py
@@ -0,0 +1,28 @@
+import pickle
+import json
+import os
+
+class CharIndex():
+    def __init__(self, file_addr):
+        f = open(file_addr)
+        formulas_str = f.readlines()
+        chars = {"آ","ب"}
+        for formula in formulas_str:
+            for char in formula:
+                if char not in chars:
+                    chars.add(char)
+        self.char_dict = {k: i for i, k in enumerate(chars)}
+        self.char_list = [None] * len(chars)
+        for i, k in enumerate(self.char_dict):
+            self.char_list[i] = k
+
+    def save(self, dir_addr):
+        handler1 = open(os.path.join(dir_addr, "char_dict.json"), "w")
+        json.dump(self.char_dict, handler1)
+        handler2 = open(os.path.join(dir_addr, "char_list.json"), "w")
+        json.dump(self.char_list, handler2)
+
+
+
+a = CharIndex("./Dataset/formulas/train_formulas.txt")
+a.save(".")
---
+++ src/Img2LatexDataset.py
@@ -0,0 +1,64 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import os
+import torch
+from PIL import Image
+import numpy as np
+from skimage import io, transform
+
+class Img2LatexDataset(Dataset):
+
+    def __init__(self, img_dir, formula_file, transform=None):
+        self.img_dir = img_dir
+        self.formula_file = formula_file
+        self.transform = transform
+        f = open(formula_file)
+        self.formulas = f.readlines()
+
+    def __len__(self):
+        return len(self.formulas)
+
+    def __getitem__(self, idx):
+        img_addr_idx = os.path.join(self.img_dir, str(idx)+".png")
+        image = io.imread(img_addr_idx)
+        formula = self.formulas[idx]
+        sample = {'image': image, 'formula':formula}
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
+
+class Rescale(object):
+
+    def __init__(self, output_size):
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, formula = sample['image'], sample['formula']
+        h,w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+        new_h, new_w = int(new_h), int(new_w)
+        img = transform.resize(image, (new_h, new_w))
+        return {'image': img, 'formula': formula}
+
+
+class ToTensor(object):
+
+    def __call__(self, sample):
+        image, formula = sample['image'], sample['formula']
+        return {'image':torch.from_numpy(image), 'formula':formula}
+
+pari_dataset = Img2LatexDataset(".././Dataset/images/images_train",".././Dataset/formulas/train_formulas.txt")
+transformed_dataset = Img2LatexDataset(".././Dataset/images/images_train",".././Dataset/formulas/train_formulas.txt",
+                                        transform=transforms.Compose([
+                                            Rescale((200, 30)),
+                                            ToTensor()
+                                        ]))
+
+x = transformed_dataset[100]
+print("Hi")
