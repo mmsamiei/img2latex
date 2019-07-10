5fdb04c56f69a65d21b0e4e54bff6a0619120863
mmsamiei <mohmahsamiei@gmail.com>
2019-07-11 01:11:01 +0430

    now we have fully token based producer probably! Ya Allah

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from src.Img2LatexDataset import *
 from src.CharIndex import *
+from src.TokenIndex import *
 from Model.model import *
 from trainer.trainer import *
 import json
 from torch.utils.data.sampler import SubsetRandomSampler
 chardict_file_addr = "./char_dict.json"
+tokendict_file_addr = "./token_dict.json"
 
 
 if __name__ =="__main__":
@@ -13,22 +15,22 @@ if __name__ =="__main__":
     transformed_dataset = Img2LatexDataset("./Dataset/images/images_train","./Dataset/formulas/train_formulas.txt",
                                                 transform=transforms.Compose([
                                                     Rescale((200, 30)),
-                                                    ToTensor("./Dataset/formulas/train_formulas.txt", "char_dict.json")
+                                                    ToTensor("./Dataset/formulas/train_formulas.txt", "token_dict.json", "token")
                                                 ]))
 
     validation_transformed_dataset = Img2LatexDataset("./Dataset/images/images_validation", "./Dataset/formulas/validation_formulas.txt",
                                            transform=transforms.Compose([
                                                Rescale((200, 30)),
-                                               ToTensor("./Dataset/formulas/validation_formulas.txt", "char_dict.json")
+                                               ToTensor("./Dataset/formulas/validation_formulas.txt", "token_dict.json", "token")
                                            ]))
 
-    dataloader = DataLoader(transformed_dataset, batch_size=batch_size, drop_last=True, sampler=SubsetRandomSampler(range(1,8000)))
-    validation_dataloader = DataLoader(validation_transformed_dataset, batch_size=batch_size, drop_last=True, sampler=SubsetRandomSampler(range(1,1000)))
+    dataloader = DataLoader(transformed_dataset, batch_size=batch_size, drop_last=True, sampler=SubsetRandomSampler(range(1,100)))
+    validation_dataloader = DataLoader(validation_transformed_dataset, batch_size=batch_size, drop_last=True, sampler=SubsetRandomSampler(range(1,100)))
     hidden_size = 256
     emb_size = 10
-    with open(chardict_file_addr) as handler:
-        char_dict = json.load(handler)
-    vocab_size = len(char_dict.keys())
+    with open(tokendict_file_addr) as handler:
+        token_dict = json.load(handler)
+    vocab_size = len(token_dict.keys())
     dev = torch.device("cpu")
 
     dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -41,17 +43,20 @@ if __name__ =="__main__":
     char_index = CharIndex()
     char_index.load('.')
 
-    trainer = Trainer(img2seq, dataloader, validation_dataloader, char_dict['خ'], dev)
+    token_index = TokenIndex()
+    token_index.load('.')
+
+    trainer = Trainer(img2seq, dataloader, validation_dataloader, token_dict['<pad>'], dev)
     trainer.init_weights()
     print("number of model parameters! : ", trainer.count_parameters())
-    trainer.train(25)
+    trainer.train(8)
 
 
     for i_batch, sample_batched in enumerate(dataloader):
          images = sample_batched['src'].to(dev)
-         result = img2seq.greedy_inference(images, char_dict['آ'], 40)
-         str = char_index.translate_to_char(result[0])
-         str = char_index.translate_to_char(result[1])
+         result = img2seq.greedy_inference(images, token_dict['<start>'], 50)
+         str = token_index.translate_to_token(result[0])
+         str = token_index.translate_to_token(result[1])
          print(str)
     # for i_batch, sample_batched in enumerate(dataloader):
     #      print(i_batch)
